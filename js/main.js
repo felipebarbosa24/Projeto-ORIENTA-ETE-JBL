1a06830a7868326bcae2be07db9522b17cc5d2ed
Jonas8233144234 <149276335+JSantosMoraes@users.noreply.github.com>
2025-07-30 21:39:13 -0300

    🚧 feat: section-development
    Construção da seção equipe no index foi iniciada.

--- js/main.js
+++ js/main.js
@@ -102,3 +102,32 @@
 
 })(jQuery);
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+});
