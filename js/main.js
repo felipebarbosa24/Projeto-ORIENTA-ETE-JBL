63a626d7ecd8dc82acd585d4f96bb9feed94da30
Akillys <akillysnascimentobnm@gmail.com>
2025-08-28 07:51:00 -0300

    :bug: fix: broken index spinner in js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,37 @@
     };
     spinner(0);
     
+    // Initiate the wowjs
+    new WOW().init();
+    
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top shadow-sm').css('top', '0px');
+        } else {
+            $('.nav-bar').removeClass('sticky-top shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: true,
+        smartSpeed: 500,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-right"></i>',
+            '<i class="bi bi-arrow-left"></i>'
+        ],
+    });
+
 //Enem Score Universitie Information Start
 
 const universidadesNotas = {
@@ -79,39 +110,6 @@ window.onclick = function(event) {
 }
 
 //Enem Scor Universitie Information End
-
-    // Initiate the wowjs
-    new WOW().init();
-    
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.nav-bar').addClass('sticky-top shadow-sm').css('top', '0px');
-        } else {
-            $('.nav-bar').removeClass('sticky-top shadow-sm').css('top', '-100px');
-        }
-    });
-
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'fadeOut',
-        items: 1,
-        margin: 0,
-        stagePadding: 0,
-        autoplay: true,
-        smartSpeed: 500,
-        dots: true,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-right"></i>',
-            '<i class="bi bi-arrow-left"></i>'
-        ],
-    });
-
-
 
     // testimonial carousel
     $(".testimonial-carousel").owlCarousel({
